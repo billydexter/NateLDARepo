96b3e9e9442c8a6d89559980086794922c80e4c6
billydexter <driggs.nathan@gmail.com>
2021-01-21 14:59:29 -0700

    Made changes to the LDAMain function to work with JSON and to filter out words better

--- NateLDARepo/FileToDocument.py
+++ NateLDARepo/FileToDocument.py
@@ -1,32 +1,62 @@
+import json
 import nltk
 import string
+from emoji.unicode_codes import UNICODE_EMOJI
 from nltk.corpus import stopwords
 
 #Turns every filename on the command line into a document to be used in LDA.
-def filesToDocuments(files):
+def filesToDocuments(tweets):
     documents = []
-    for file in files:
-        documents.append(oneFileToDocument(file))
+    #sets the stopwords once so you don't have to do it every iteration. For performance.
+    stop_words = set(stopwords.words('english'))
+    for tweet in tweets:
+        documents.append(oneFileToDocument(tweet, stop_words))
     return documents
 
 #Cleans the documents of any unnecessary words, punctuation, or numbers.
-def oneFileToDocument(file):
-    #Turns file into one string.
-    text = open(file).read()
+def oneFileToDocument(tweet, stop_words):
     #Gets rid of numbers.
-    text = nltk.re.sub(r'\d+', '', text)
+    text = nltk.re.sub(r'\d+', '', tweet)
     #Gets rid of punctuation and turns things into lowercase.
-    text = "".join([char.lower() for char in text if char not in string.punctuation])
-    words = nltk.word_tokenize(text)
-    words = [w.lower() for w in words]
-    stop_words = set(stopwords.words('english'))
+    words = text.split()
+    #The loop changing special characters.
+    for i in range(0, len(words)):
+        if (words[i][0] == "@"):
+            words[i] = "[MENTION]"
+        elif (words[i][0] == "#"):
+            words[i] = "[HASHTAG]"
+        elif (len(words[i]) > 1 and words[i][0:4] == "http"):
+            words[i] = "[URL]"
+        elif (len(words[i]) > 14 and words[i][0:15] == "pic.twitter.com"):
+            words[i] = "[PICTURE]"
+        else:
+            words[i] = deEmojify(words[i].lower(), words)
+    #Gets rid of punctuation marks.
+    table = str.maketrans('', '', string.punctuation)
+    words = [w.translate(table) for w in words]
+    #Gets rid of stop words and words that are less than 3 letters long.
+    words = [w for w in words if not w in stop_words and len(w) > 2]
 
-    filtered_sentence = [w for w in words if not w in stop_words]
+    return words
 
-    filtered_sentence = []
+#Turns a JSON file into a list of tweets.
+def extractJSON(file):
+    with open(file) as file:
+        data = json.load(file)
+        tweetList = list(data)
+        return tweetList
 
-    #Gets rid of stop words like "The" or "are".
-    for w in words:
-        if w not in stop_words and w not in "“":
-            filtered_sentence.append(w)
-    return filtered_sentence
+#Detects unusual ascii characters and will save them as emojis if needed.
+def deEmojify(inputString, words):
+    if (inputString.encode('ascii', 'ignore').decode('ascii') != inputString):
+        for letter in inputString:
+            testEmojiCharacter(letter, words)
+    return inputString.encode('ascii', 'ignore').decode('ascii')
+
+#Detects if a character is an emoji, and if so, it adds it to the end of the list.
+def testEmojiCharacter(emojiCharacter, words):
+    try:
+        emojiTest = UNICODE_EMOJI[emojiCharacter]
+        words.append(emojiTest + "emoji")
+    except KeyError:
+        pass
--- NateLDARepo/LDAMain.py
+++ NateLDARepo/LDAMain.py
@@ -4,24 +4,26 @@
 
 #if the import statements don't work, try from NateLDARepo.FileToDocument import filesToDocuments
 #for each level.
-from FileToDocument import filesToDocuments
-from PoissonSampling import PoissonSampling
-from SampleData import getSampleData1
-from WordSample import WordSample
+from FileToDocument import filesToDocuments, extractJSON
+
 import sys
 
-poissonSampler = PoissonSampling()
-wordSampler = WordSample()
+#TODO: Exclude tweets that are marked as "INVALID"
+
 files = []
 #Get the file arguments from the command line.
 for i in range(1, len(sys.argv)):
     files.append(sys.argv[i])
-#Turns the files into separated strings of text.
-documents = filesToDocuments(files)
-#Gets the number of words from each documents based on Poisson Distribution.
-wordCounts = poissonSampler.getPoissonSampleNumber(documents)
-#Randomly samples words from the document
-words = wordSampler.getRandomWordsForEachDocument(wordCounts, documents)
-#prints the words to the console. Not the end goal, but just to see what it's like.
-print(words)
+if len(files) < 2:
+    print("Include the indian and american JSON files in the command line arguments.")
+    exit()
+#extract JSON turns the files into a list of strings of text.
+indianTweets = extractJSON(files[0])
+#format and clean the words from the JSON file.
+indianDocuments = filesToDocuments(indianTweets)
+americanTweets = extractJSON(files[1])
+americanDocuments = filesToDocuments(americanTweets)
+
+#Put a breakpoint before this line to see the data.
+print("Done")
 
